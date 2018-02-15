92135838e92f92044b68276e30f40811e3ebabf0
zeke <zekevictor@gmail.com>
2018-02-14 18:45:02 -0800

    the function

---
+++ src/getBlobDuration.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Blob} blob
+ *
+ * @returns {Promise<Number>} Blob duration in seconds.
+ */
+export default async function getBlobDuration(blob) {
+  const tempVideoEl = document.createElement('video')
+
+  const durationP = new Promise(resolve =>
+    tempVideoEl.addEventListener('loadedmetadata', () => {
+      // Chrome bug: https://bugs.chromium.org/p/chromium/issues/detail?id=642012
+      if(tempVideoEl.duration === Infinity) {
+        tempVideoEl.currentTime = Number.MAX_SAFE_INTEGER
+        tempVideoEl.ontimeupdate = () => {
+          tempVideoEl.ontimeupdate = null
+          resolve(tempVideoEl.duration)
+          tempVideoEl.currentTime = 0
+        }
+      }
+      // Normal behavior
+      else
+        resolve(tempVideoEl.duration)
+    })
+  )
+
+  tempVideoEl.src = window.URL.createObjectURL(blob)
+
+  return durationP
+}
