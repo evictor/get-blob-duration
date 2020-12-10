4b9622258fc0e5f5c05e790446f29f16de8d5323
Stepan Bolotnikov <stepan@snackable.ai>
2020-12-10 14:30:44 +0200

    Moved the error handler to a more appropriate place

--- src/getBlobDuration.js
+++ src/getBlobDuration.js
@@ -6,7 +6,7 @@
 export default async function getBlobDuration(blob) {
   const tempVideoEl = document.createElement('video')
 
-  const durationP = new Promise((resolve, reject) =>
+  const durationP = new Promise((resolve, reject) => {
     tempVideoEl.addEventListener('loadedmetadata', () => {
       // Chrome bug: https://bugs.chromium.org/p/chromium/issues/detail?id=642012
       if(tempVideoEl.duration === Infinity) {
@@ -16,15 +16,15 @@ export default async function getBlobDuration(blob) {
           resolve(tempVideoEl.duration)
           tempVideoEl.currentTime = 0
         }
-        tempVideoEl.addEventListener('error', (event) =>
-          reject(event.target.error)
-        )
       }
       // Normal behavior
       else
         resolve(tempVideoEl.duration)
-    }),
-  )
+    })
+    tempVideoEl.addEventListener('error', (event) =>
+      reject(event.target.error)
+    )
+  })
 
   tempVideoEl.src = typeof blob === 'string' || blob instanceof String
     ? blob
